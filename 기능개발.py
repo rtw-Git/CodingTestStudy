2720ea29821a550dafd28c0206bdd84d5572da6d
SuJung <101905565+rtw-Git@users.noreply.github.com>
2022-04-09 19:22:34 +0900

    Update 기능개발.py

--- 기능개발.py
+++ 기능개발.py
@@ -1,11 +1,11 @@
 def solution(progresses, speeds):
     answer = []         #result_cnt
-    result_day = [] 
-    n = 1       #n일째
+    result_days = [] 
+    n = 0       #n일째
     start = 0   #return_list의 인덱스, 연속을 확인하기 위함
     new_progresses = [0 for i in range(len(progresses))] 
     
-    while new_progresses != [1 for i in range(len(progresses))]: 
+    while new_progresses != [-1 for i in range(len(progresses))]: 
         for i in range(start, len(progresses)):
                 new_progresses[i] = progresses[i] +speeds[i] * n
         
